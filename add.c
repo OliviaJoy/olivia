d3a2d39f50069827cfaeeedb2429f43275387503
test2-66 <dbit@Abhay>
2016-08-10 17:16:28 +0530

    Changed to multiplication

--- add.c
+++ add.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 main()
 {
-int a=178,b=6,c,d;
-c=a+b;
+int a=15,b=6,c,d;
+c=a*b;
 d=a-b;
 
-printf("The sum is %d \n",c);
+printf("The product is %d \n",c);
 printf("The diff is %d \n",d);
 }
 
